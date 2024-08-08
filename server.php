3fa60f8f4d11d428edc24f644f579fb8bed2684c
Sowndharbharathi <146612570+Sowndharbharathi@users.noreply.github.com>
2024-08-08 10:07:21 +0530

    color change

--- server.php
+++ server.php
@@ -7,7 +7,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'project');
+$db = mysqli_connect('localhost', 'root', '', 'DBVAULT');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
